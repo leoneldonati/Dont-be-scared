3ab4180623264d3321b7acf52764d222d86882c7
Leonel Donati <leonelroman321@gmail.com>
2024-10-08 16:44:26 -0300

    added React, Tailwind, Astro DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import netlify from "@astrojs/netlify";
 
+import db from "@astrojs/db";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [db(), react(), tailwind()]
 });
